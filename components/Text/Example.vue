3a6bb673988e0d070367a1c696f27baf32752fac
samiebuka <samuel.ebuka@gmail.com>
2024-01-07 14:02:27 +0100

    Sample Text Component

---
+++ components/Text/Example.vue
@@ -0,0 +1,75 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="grid gap-5 md:grid-cols-2 w-full max-w-5xl mx-auto">
+    <!-- Primary Section -->
+    <div class="bg-primary text-primary-content p-5">
+        <h1>Primary Section</h1>
+        <p>This is a text in the primary section with primary content color.</p>
+    </div>
+
+    <!-- Secondary Section -->
+    <div class="bg-secondary text-secondary-content p-5">
+        <h2>Secondary Section</h2>
+        <p>This is a text in the secondary section with secondary content color.</p>
+    </div>
+
+        <!-- Accent Section -->
+        <div class="bg-accent text-accent-content p-5">
+        <h2>Accent Section</h2>
+        <p>This is a text in the accent section with accent content color.</p>
+    </div>
+
+    <!-- Neutral Section -->
+    <div class="bg-neutral text-neutral-content p-5">
+        <h2>Neutral Section</h2>
+        <p>This is a text in the neutral section with neutral content color.</p>
+    </div>
+
+    <!-- Base 100 Section -->
+    <div class="bg-base-100 text-base-content p-5">
+        <h2>Base 100 Section</h2>
+        <p>This is a text on base 100 color background with base content color.</p>
+    </div>
+
+    <!-- Base 200 Section -->
+    <div class="bg-base-200 text-base-content p-5">
+        <h2>Base 200 Section</h2>
+        <p>This is a text on base 200 color background with base content color.</p>
+    </div>
+
+    <!-- Base 300 Section -->
+    <div class="bg-base-300 text-base-content p-5">
+        <h2>Base 300 Section</h2>
+        <p>This is a text on base 300 color background with base content color.</p>
+    </div>
+
+        <!-- Info Section -->
+        <div class="bg-info text-info-content p-5">
+        <h2>Info Section</h2>
+        <p>This is a text in the info section with info content color.</p>
+    </div>
+
+    <!-- Success Section -->
+    <div class="bg-success text-success-content p-5">
+        <h2>Success Section</h2>
+        <p>This is a text in the success section with success content color.</p>
+    </div>
+
+    <!-- Warning Section -->
+    <div class="bg-warning text-warning-content p-5">
+        <h2>Warning Section</h2>
+        <p>This is a text in the warning section with warning content color.</p>
+    </div>
+
+    <!-- Error Section -->
+    <div class="bg-error text-error-content p-5">
+        <h2>Error Section</h2>
+        <p>This is a text in the error section with error content color.</p>
+    </div>
+
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="p-5 w-full">
+    <h1 class="w-full text-4xl text-center p-5">Text Example</h1>
+    <TextExample />
+  </div>
+</template>
+
+<style scoped></style>
